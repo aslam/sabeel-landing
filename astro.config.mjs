9d914cd7eb9c0b054c308598a6e760f9fefd3a82
Syed Aslam <aslam.maqsood@gmail.com>
2025-05-16 03:12:53 +0530

    use Partytown for gtag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sabeel.in',
   integrations: [
-    partytown({ config: { forward: ['dataLayer.push'] } }),
+    partytown({
+      config: {
+        forward: ['dataLayer.push', 'gtag'],
+        resolveUrl: (url) => {
+          if (url.hostname === 'www.googletagmanager.com') {
+            return new URL(
+              'https://www.googletagmanager.com' + url.pathname
+            );
+          }
+          return url;
+        },
+      },
+    }),
   ],
 });
