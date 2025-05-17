19febab32b9de3b79a7e307417ced1a1b10c9af4
Syed Aslam <aslam.maqsood@gmail.com>
2025-05-17 15:01:32 +0530

    remove partytown and use astro-analytics component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,42 +1,42 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import partytown from '@astrojs/partytown';
+// import { defineConfig } from 'astro/config';
+// import partytown from '@astrojs/partytown';
 
-// https://astro.build/config
-export default defineConfig({
-  site: 'https://sabeel.in',
-  integrations: [
-    partytown({
-      config: {
-        forward: ['dataLayer.push', 'gtag'],
-        // Only enable debug mode in development
-        debug: false,
-        resolveUrl: (url) => {
-          const gtmUrls = [
-            'www.googletagmanager.com',
-            'googletagmanager.com',
-            'analytics.google.com',
-            'www.google-analytics.com',
-          ];
+// // https://astro.build/config
+// export default defineConfig({
+//   site: 'https://sabeel.in',
+//   integrations: [
+//     partytown({
+//       config: {
+//         forward: ['dataLayer.push', 'gtag'],
+//         // Only enable debug mode in development
+//         debug: false,
+//         resolveUrl: (url) => {
+//           const gtmUrls = [
+//             'www.googletagmanager.com',
+//             'googletagmanager.com',
+//             'analytics.google.com',
+//             'www.google-analytics.com',
+//           ];
 
-          if (gtmUrls.includes(url.hostname)) {
-            const finalUrl = new URL(
-              `https://${url.hostname}${url.pathname}${url.search}`
-            );
-            // Set no-cors mode for debug endpoints
-            if (url.pathname.includes('/debug/')) {
-              return {
-                url: finalUrl,
-                options: {
-                  mode: 'no-cors',
-                },
-              };
-            }
-            return finalUrl;
-          }
-          return url;
-        },
-      },
-    }),
-  ],
-});
+//           if (gtmUrls.includes(url.hostname)) {
+//             const finalUrl = new URL(
+//               `https://${url.hostname}${url.pathname}${url.search}`
+//             );
+//             // Set no-cors mode for debug endpoints
+//             if (url.pathname.includes('/debug/')) {
+//               return {
+//                 url: finalUrl,
+//                 options: {
+//                   mode: 'no-cors',
+//                 },
+//               };
+//             }
+//             return finalUrl;
+//           }
+//           return url;
+//         },
+//       },
+//     }),
+//   ],
+// });
