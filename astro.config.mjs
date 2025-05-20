6b611bd031310a7b1558800d47895a56ef16771a
Syed Aslam <aslam.maqsood@gmail.com>
2025-05-20 17:35:10 +0530

    add Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,42 +1,15 @@
 // @ts-check
-// import { defineConfig } from 'astro/config';
-// import partytown from '@astrojs/partytown';
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// // https://astro.build/config
-// export default defineConfig({
-//   site: 'https://sabeel.in',
-//   integrations: [
-//     partytown({
-//       config: {
-//         forward: ['dataLayer.push', 'gtag'],
-//         // Only enable debug mode in development
-//         debug: false,
-//         resolveUrl: (url) => {
-//           const gtmUrls = [
-//             'www.googletagmanager.com',
-//             'googletagmanager.com',
-//             'analytics.google.com',
-//             'www.google-analytics.com',
-//           ];
-
-//           if (gtmUrls.includes(url.hostname)) {
-//             const finalUrl = new URL(
-//               `https://${url.hostname}${url.pathname}${url.search}`
-//             );
-//             // Set no-cors mode for debug endpoints
-//             if (url.pathname.includes('/debug/')) {
-//               return {
-//                 url: finalUrl,
-//                 options: {
-//                   mode: 'no-cors',
-//                 },
-//               };
-//             }
-//             return finalUrl;
-//           }
-//           return url;
-//         },
-//       },
-//     }),
-//   ],
-// });
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://sabeel.in',
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
+});
