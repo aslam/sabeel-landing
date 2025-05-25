200cd207eed7ed37ca1195ed97d3abe2957b6643
Syed Aslam <aslam.maqsood@gmail.com>
2025-05-25 19:48:47 +0530

    add Tailwind CSS
    - remove partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sabeel.in',
   integrations: [
+    tailwind(),
     sitemap({
       changefreq: 'weekly',
       priority: 0.7,
